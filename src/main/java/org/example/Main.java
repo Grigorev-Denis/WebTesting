28fb7898447a26679b017e036744f5a40fe6f436
d.grigorev <freestylemilll@gmail.com>
2025-04-30 10:36:49 +0300

    модуль13дз

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
+        Boxes box1 = new Boxes(10,10,10);
+        Boxes box2 = new Boxes(20,20,20);
+        Boxes box3 = new Boxes(40,40,40);
 
-        System.out.println("Hello world!");
+        ArrayList <Boxes> boxList = new ArrayList<>();
 
+        boxList.add(box1);
+        boxList.add(box2);
+        boxList.add(box3);
+
+        Main mn = new Main();
+        System.out.println(mn.getBigBoxList(boxList).size());
+
+    }
+
+    public ArrayList<Boxes> getBigBoxList(ArrayList <Boxes> boxes){
+        ArrayList <Boxes> bigBoxes = new ArrayList<>(boxes);
+        ListIterator <Boxes> iterator = bigBoxes.listIterator();
+        while (iterator.hasNext()){
+            if(iterator.next().getWidth()<= 30){
+                iterator.remove();
+            }
+        }
+        return bigBoxes;
     }
 }
